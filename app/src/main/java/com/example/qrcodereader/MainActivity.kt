b591259e90c1461db8d37397c7e32f1888e5240d
Sergey <68073864+seregatheone@users.noreply.github.com>
2021-10-12 21:30:45 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/qrcodereader/MainActivity.kt
+++ app/src/main/java/com/example/qrcodereader/MainActivity.kt
@@ -7,10 +7,6 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
-
-    private  var navHostFragment : NavHostFragment? = null
-    private var navController: NavController? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
